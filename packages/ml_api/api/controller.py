b0a004fe242f02956217d96d9b18ba430c3739f2
Neal Gilmore <neal.gilmore@gmail.com>
2020-01-10 13:04:54 -0500

    added the prediction endpoint

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from regression_model.predict import make_prediction
+
+from api.config import get_logger
+
+_logger = get_logger(logger_name=__name__)
 
 
 prediction_app = Blueprint('prediction_app', __name__)
@@ -7,4 +12,21 @@ prediction_app = Blueprint('prediction_app', __name__)
 @prediction_app.route('/health', methods=['GET'])
 def health():
     if request.method == 'GET':
+        _logger.info('health status OK')
         return 'ok'
+
+
+@prediction_app.route('/v1/predict/regression', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        json_data = request.get_json()
+        _logger.info(f'Inputs: {json_data}')
+
+        result = make_prediction(input_data=json_data)
+        _logger.info(f'Outputs: {result}')
+
+        predictions = result.get('predictions')[0]
+        version = result.get('version')
+
+        return jsonify({'predictions': predictions,
+                        'version': version})
